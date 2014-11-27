c82cf88c8cddc41dd07abf9c2e19ca37afea00ca
Yasser Afifi <yasser@Yassers-MacBook-Pro.local>
2014-11-27 12:07:13 +0000

    FINAL AFTER MODIFICATION

--- Server.java
+++ Server.java
@@ -1,6 +1,9 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -16,15 +19,17 @@ public class Server implements Runnable {
 	}
 
 	public static void main(String[] args) {
-		  if(args.length != 1) {
-	            System.err.println("Error! Incorrect arguments! Please try again by typing TCP port number as the only argument");
-	            System.exit(1);
-	        }
-		int port = Integer.parseInt(args[0]);
+		
+		if(args.length != 1) {
+        System.err.println("Error! Incorrect arguments! Please try again by typing TCP port number as the only argument");
+        System.exit(1);
+       }
+	int port = Integer.parseInt(args[0]);
+
 		ExecutorService threadGenerator = Executors.newFixedThreadPool(10);// thread generator up to 10 threads
 		Socket clientSocket = null;
 
-		try (ServerSocket serverSocket = new ServerSocket(port);) {
+		try (ServerSocket serverSocket = new ServerSocket(9876);) {
 			while (!Thread.currentThread().isInterrupted()) {
 				try {
 					clientSocket = serverSocket.accept();// create a client socket
@@ -42,32 +47,36 @@ public class Server implements Runnable {
 	// ------------------- Handling clients----------------
 	@Override
 	public void run() {
-		try (DataInputStream fromClient = new DataInputStream(
-				clientSocket.getInputStream());
-				DataOutputStream toClient = new DataOutputStream(
-						clientSocket.getOutputStream());) {
+		try (BufferedReader fromClient = new BufferedReader(new InputStreamReader (clientSocket.getInputStream()));
+				PrintWriter toClient = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()),true);) {
 			while (true) {
-				String clientText = fromClient.readUTF(); // read client text
+				String clientText = fromClient.readLine(); // read client text
 				if (clientText.equals("KILL_SERVICE")) { // end service if text contains KILL_SERVICE
-					toClient.writeUTF("Server terminated \n");
-					toClient.flush();
-					System.exit(1);
+					break;
 				} else if (clientText.contains("HELO")) {
 					String helo = clientText + "\n"
-							+ " IP : "+ clientSocket.getLocalAddress() + "\n" 
-							+"Port: "+ clientSocket.getLocalPort() + "\n"
-							+ "StudentID: 14303154";
-					toClient.writeUTF(helo);
-					toClient.flush();
+							+ "IP:"+ InetAddress.getLocalHost().getHostAddress() + "\n" 
+							+"Port:"+ clientSocket.getLocalPort() + "\n"
+							+ "StudentID:14303154";
+					toClient.println(helo);
 				} else { // Echo Client text
-					toClient.writeUTF(clientText);
-					toClient.flush();
+					toClient.println(clientText);
 				}// end else
+				
+				clientSocket.close();
+				fromClient.close();
+				toClient.close();
+				System.exit(0);
+				
 			}// end while
+			
+			
+			
+			
 		} // end try
 		catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("\nError in Server\n");
 		}// end catch
 	} // end run
-}// end class
+}// 
